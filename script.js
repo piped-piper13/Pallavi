b4380cd8bb3c28f8c6085bdb12912a644c235b19
piped-piper13 <pallavids1113@gmail.com>
2026-06-26 10:36:00 +0530

    Add initial gossip entries to script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+[
+    {
+        "alias": "GossipGirl",
+        "date": "26/06/2026, 10:00:00 AM",
+        "text": "You will not believe what happened in the cafeteria today. Complete chaos."
+    },
+    {
+        "alias": "AnonymousTea",
+        "date": "25/06/2026, 09:15:00 PM",
+        "text": "Welcome to our secret drama folder! Keep it locked."
+    }
+]
